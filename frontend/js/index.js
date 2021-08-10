4063caa31d1f7548f6ef4decd01aac2d41272436
Ana Nasta <univpro81@gmail.com>
2021-08-10 16:43:08 +0200

    promesse objet index

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -16,6 +16,7 @@ return response.json();
 });
 */
 
+/*
 
 
 const url = 'http://localhost:3000/api/teddies';
@@ -30,3 +31,36 @@ async function getBears () {
 
 getBears();
 
+
+*/
+
+/*
+
+fetch("http://localhost:3000/api/teddies")
+    .then(response => response.json())
+    .then(peluches => {
+        const ul = document.getElementById('peluches');
+        peluches.forEach(peluche => {
+             peluche = new Peluches(peluche);
+             peluche.setContainer(ul);
+             peluche.display('peluches');
+        })
+    })
+    .catch(function (error){
+        console.log(error)
+    });
+
+    */
+
+    const getBears = async function () {
+        let response = await fetch ('http://localhost:3000/api/teddies')
+        if (response.ok) {
+             let data = await response.json()
+        console.log (data);
+        } else {
+            console.error('Retour du serveur' , response.status)
+        }
+       
+    }
+
+    getBears();
