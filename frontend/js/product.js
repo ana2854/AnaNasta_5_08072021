6896c4e29c798d3f25557ee09b48ba3fc1ff6a97
Ana Nasta <univpro81@gmail.com>
2021-08-19 15:54:38 +0200

    liens pages du site

--- frontend/js/product.js
+++ frontend/js/product.js
@@ -1,12 +1,12 @@
 
 
-/*function getProduit () {
+function getProduit () {
     const params = new URLSearchParams(window.location.search);
 
     console.log(params);
 
 }
-*/
+
 
 const queryString = 'id=5beaaa8f1c9d440000a57d95';
 
@@ -21,6 +21,9 @@ console.log(`Value for 'id' is : ${idProduct}`)
 for (const [key, value] of a) {
     console.log(`${key} => ${value}`)
 }
+
+
+
 
 
 
