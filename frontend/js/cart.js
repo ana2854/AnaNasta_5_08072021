f3c1162e7f92e38dca60b487845fa735661c1a39
Ana Nasta <univpro81@gmail.com>
2021-09-07 11:30:26 +0200

    form stylisation

--- frontend/js/cart.js
+++ frontend/js/cart.js
@@ -1,6 +1,6 @@
 
 
-     // si le local storage n'est pas vide
+     // si le local storage n'est pas vide alors afficher les produits choisis dans le panier
        if (typeof(Storage) !== "undefined") {
        // récupère les datas 
        affichageProduitsChoisis()
@@ -44,6 +44,7 @@
           }
         }
 
+        // FONCTION POUR LE PRIX TOTAL
         function affichagePrixTotal () {
             let prixTotal = document.getElementById("total-price")
             let divPrixTotal = document.createElement("div");
@@ -73,172 +74,55 @@
 
         function formulaire () {
 
-          let form = document.getElementById("form");
-
-          content = '';
-
-          content += 
-          ` 
-          <form id="formbox" name="formbox" action="/" method="GET" >
-          <p>Remplissez le formulaire</p>
-
-            <div class="form_item">
-              <label for="firstname">
-              Prénom
-              <input id="firstname" name="firstname" type="text" placeholder="Prénom" required>
-              </label>
-              <small> </small>
-            </div>
-
-            <div class="form_item">
-              <label for="lastname">
-              Nom de famille
-              <input id="lastname" name="lastname" type="text" placeholder="Nom de Famille">
-              </label>
-              <small ></small>
-            </div>
-
-            <div class="form_item">
-              <label for="adress">
-              Adresse
-              <input id="adress" name="adress" type="text" placeholder="Adresse">
-              </label>
-              <small></small>
-            </div>
-
-            <div class="form_item">
-              <label for="city">
-              Ville
-              <input id="city" name="city" type="text" placeholder="Ville">
-              </label>
-              <small></small>
-            </div>
-
-            <div class="form_item">       
-            <label for="email">
-            Email
-            <input id="email" type="email" name="email" type="email" placeholder="e-mail" >
-            <small></small>
-            </label>
-            </div>
-
-            
-            <div class="commander">
-              <button id="btn-commander" type="submit">Commander</button>
-          </div>
-
-           </form>
-
-           <p class="error"</p>
-          `;
-
-          form.innerHTML = content;
-
-        
-         document.forms["formbox"].addEventListener("submit", function(e) {
-           let erreur;
-
-           let inputs = this;
-
-           for (var i = 0; i < inputs.length; i++) {
-             console.log(inputs[i]);
-             if (!inputs[i].value) {
-               erreur = "veuillez renseignez tous les champs"
-             }
-           }
-
-           if (erreur) {
-             e.preventDefault();
-             document.getElementsByClassName("error").innerHTML=erreur;
-             return false
-           } else {
-             alert('formulaire envoyé');
-           }
-         })
        
-      
-
-          /*selection des input du formulaire
+        //selection des input du formulaire
          const firstname = document.getElementById("firstname");
-         const lastname = document.getElementById("lastname");
+         const lastname = document.getElementById("lastname").value;
          const adress = document.getElementById("adress");
          const city = document.getElementById("city");
+         let formbox = document.getElementById('formbox')
+
+
+         let smallMail = document.getElementById("errorEmail")
+         let smallLastName = document.getElementById("errorLastName")
          
-         let small = document.getElementsByTagName("small")
+         let msgError = document.getElementsByClassName("error")
+         let btnCommander = document.getElementById("btn-commander")
+        
+         let input = document.getElementsByTagName("input")
 
        
-          let email = document.getElementById("email").value;
 
-
+        // ADD EVENT LISTENER sur bouton commander
+        btnCommander.addEventListener('click', function(e) {
           
-
-
-          /*
-
-          email.addEventListener("change", function() {
-
-          let regx = /^[a-zA-Z0-9.-_]+[@]{1}[a-zA-z0-9.-_]+[.]{1}[a-z]{2,10}$'/
-
-          if(regx.text(email)) {
-            small.innerHTML = "Adresse mail valide";
-            small.classList.remove("error");
-            small.classList.add("ok")
-      } else {
-          small.innerHTML = "Adresse mail non valide";
-          small.classList.remove("ok");
-          small.classList.add("error");
-        }
-          })
-        }
-      
-         /*
-         console.log(email)
-          console.log(lastname)
-
-         formbox.addEventListener("submit", function(e){
-           e.preventDefault();
-           let messages = []
-           if (adress.value === null ) {
-             messages.push('name is required')}
-           }
-         )}
-
-*/
-
-
-
-/*
-         //on écoute la case input est modifié et on lui indique quoi faire
-         formbox.email.addEventListener("onInput", function () {
-           validEmail(this);
-         });
-
-         console.log(formbox.email)
+        let email = document.getElementById("email").value;
+        e.preventDefault();
+        console.log(email)
         
+        //REGEX EMAIL 
+        let regxMail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
-        const validEmail = function(inputEmail) {
-          //creation de la regex exp pour validation email
-          let emailRegExp = new RegExp ('^[a-zA-Z0-9.-_]+[@]{1}[a-zA-z0-9.-_]+[.]{1}[a-z]{2,10}$', 'g');
-        };
-
-        //récupération de la balise small
-        let small = inputEmail.nextElementSibling;
-        
-        console.log(small)
-
-      
-      //test de l'expression régulière
-          if(emailRegExp.test(inputEmail.value)) {
-              small.innerHTML = "Adresse mail valide";
-              small.classList.remove("error");
-              small.classList.add("ok")
+        if(regxMail.test(email)) {
+          smallMail.innerHTML = "Adresse mail valide";
+          smallMail.classList.remove("error");
+          smallMail.classList.add("ok")
         } else {
-            small.innerHTML = "Adresse mail non valide";
-            small.classList.remove("ok");
-            small.classList.add("error");
-          }
+        smallMail.innerHTML = "Adresse mail non valide";
+        smallMail.classList.remove("ok");
+        smallMail.classList.add("error");
         }
-          
-        */
+        
+        // REGEX NOM DE FAMILLE 
+        let regexPrenomNomVille = /^[a-zA-Z]+ [a-zA-Z]+$/
 
-        }
+        if(regexPrenomNomVille.test(lastname)) {
+          smallLastName.innerHTML = "Nom valide";
+          smallLastName.classList.remove("error");
+          smallLastName.classList.add("ok")
+        }else {
+        smallLastName.innerHTML = "Le nom n'est pas valide";
+        smallLastName.classList.remove("ok");
+        smallLastName.classList.add("error");
+      }
+        })}
